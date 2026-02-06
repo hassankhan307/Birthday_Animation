35a3e43064cf0a639481c996ab7769b2bad47280
hassankhan307 <102529784+hassankhan307@users.noreply.github.com>
2026-02-06 06:49:17 +0500

    Include a name in birthday greeting strings

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY!'], // <- Here you can add your name!
+			strings: [ 'HAPPY', 'BIRTHDAY!' , 'FATIMA'], // <- Here you can add your name!
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
